8da0fb04a2fc2d7451cfa39608d0f07b28e2c65f
DekiRostilj7 <dejan.rosulj89@gmail.com>
2019-12-27 17:47:26 +0100

    filter

--- src/components/ShopSearch.vue
+++ src/components/ShopSearch.vue
@@ -1,8 +1,13 @@
 <template>
   <div>
     <form class="form-inline mt-2 mt-md-0" @submit.prevent="submit">
-      <input class="form-control mr-sm-2" type="text" placeholder="Filter" aria-label="Search" />
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit" @click="setSearchTerm({ term: $event.target.value})">Filtriraj</button>
+      <input
+        class="form-control mr-sm-2"
+        type="text"
+        placeholder="Search"
+        aria-label="Search"
+        @input="setSearchTerm({ term: $event.target.value})"
+      />
     </form>
   </div>
 </template>
@@ -15,5 +20,5 @@ export default {
       setSearchTerm: "setSearchTerm"
     })
   }
-}
+};
 </script>
